223dbcb368abfaaf060364def9cd65bff69068d8
Mohamed Elmoghany <m.osama.elmoghany@gmail.com>
2025-03-26 22:56:41 +0200

    new papers

--- translators/OhioLINK.js
+++ translators/OhioLINK.js
@@ -1,3 +1,43 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:70db4755dc3394cf66ab2f4e3a2a552e6dd3bf19b57e58a42289267600d4aca2
-size 1075
+{
+	"translatorID": "17c974e6-7c4f-4ac8-93af-8d74debca110",
+	"label": "OhioLINK",
+	"creator": "Aurimas Vinckevicius",
+	"target": "^https?://journals\\.ohiolink\\.edu/ejc/article\\.cgi\\?",
+	"minVersion": "3.0",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcs",
+	"lastUpdated": "2017-01-01 15:28:48"
+}
+
+function detectWeb(doc, url) {
+	if (getRISLink(doc)) return "journalArticle";
+}
+
+function getRISLink(doc) {
+	return ZU.xpathText(doc, '//p[@id="ris-export"]/a/@href');
+}
+
+function doWeb(doc, url) {
+	ZU.doGet(getRISLink(doc), function(text) {
+		var trans = Zotero.loadTranslator('import');
+		// RIS
+		trans.setTranslator('32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7');
+		trans.setString(text);
+		trans.setHandler('itemDone', function(obj, item) {
+			var pdf = ZU.xpath(doc, '//div[@id="manifest"]/a[contains(@title,"PDF")]')[0];
+			if (pdf) {
+				item.attachments.push({
+					title: 'Full Text PDF',
+					url: pdf.href,
+					mimeType: 'application/pdf'
+				});
+			}
+			
+			item.complete();
+		});
+		trans.translate();
+	});
+}
