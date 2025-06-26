3ebc1b72c5cf847d8c533202e99cc4e8024b8acf
Mohamed Elmoghany <melmoghany@adobe.com>
2025-06-26 16:03:41 -0700

    added papers

--- translators/Citation graph.js
+++ translators/Citation graph.js
@@ -16,10 +16,10 @@
 	},
 	"configOptions": {
 		"getCollections": true,
-		"hash": "edcdab0ca83657dcc82de2f3e8c15de9b53954993e358d6ba0dbbcef7d4624db"
+		"hash": "66de1dec9db558de0fef27ff3985acb16fdb7e2d4c8d030a7e52a3a6e074928b"
 	},
 	"priority": 100,
-	"lastUpdated": "2025-05-26"
+	"lastUpdated": "2025-06-24"
 }
 
 ZOTERO_CONFIG = {"GUID":"zotero@zotero.org","ID":"zotero","CLIENT_NAME":"Zotero","DOMAIN_NAME":"zotero.org","PRODUCER":"Digital Scholar","PRODUCER_URL":"https://digitalscholar.org","REPOSITORY_URL":"https://repo.zotero.org/repo/","BASE_URI":"http://zotero.org/","WWW_BASE_URL":"https://www.zotero.org/","PROXY_AUTH_URL":"https://zoteroproxycheck.s3.amazonaws.com/test","API_URL":"https://api.zotero.org/","STREAMING_URL":"wss://stream.zotero.org/","SERVICES_URL":"https://services.zotero.org/","API_VERSION":3,"CONNECTOR_MIN_VERSION":"5.0.39","PREF_BRANCH":"extensions.zotero.","BOOKMARKLET_ORIGIN":"https://www.zotero.org","BOOKMARKLET_URL":"https://www.zotero.org/bookmarklet/","START_URL":"https://www.zotero.org/start","QUICK_START_URL":"https://www.zotero.org/support/quick_start_guide","PDF_TOOLS_URL":"https://www.zotero.org/download/xpdf/","SUPPORT_URL":"https://www.zotero.org/support/","SYNC_INFO_URL":"https://www.zotero.org/support/sync","TROUBLESHOOTING_URL":"https://www.zotero.org/support/getting_help","FEEDBACK_URL":"https://forums.zotero.org/","CONNECTORS_URL":"https://www.zotero.org/download/connectors","CHANGELOG_URL":"https://www.zotero.org/support/changelog","CREDITS_URL":"https://www.zotero.org/support/credits_and_acknowledgments","LICENSING_URL":"https://www.zotero.org/support/licensing","GET_INVOLVED_URL":"https://www.zotero.org/getinvolved","DICTIONARIES_URL":"https://download.zotero.org/dictionaries/","PLUGINS_URL":"https://www.zotero.org/support/plugins","NEW_FEATURES_URL":"https://www.zotero.org/blog/zotero-7/"}
@@ -1636,7 +1636,7 @@ ${obj.stack}]`;
       return "{object}";
     };
   }
-  function stringify(obj, indent) {
+  function stringify(obj, indent = 2) {
     return JSON.stringify(obj, replacer(), indent);
   }
   function to_s(obj) {
--- translators/YouTube.js
+++ translators/YouTube.js
@@ -1,15 +1,15 @@
 {
 	"translatorID": "d3b1d34c-f8a1-43bb-9dd6-27aa6403b217",
+	"translatorType": 4,
 	"label": "YouTube",
 	"creator": "Sean Takats, Michael Berkowitz, Matt Burton, Rintze Zelle, and Geoff Banh",
 	"target": "^https?://([^/]+\\.)?youtube\\.com/",
 	"minVersion": "3.0",
-	"maxVersion": "",
+	"maxVersion": null,
 	"priority": 100,
 	"inRepository": true,
-	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2024-04-05 04:04:37"
+	"lastUpdated": "2025-06-12 16:15:00"
 }
 
 /*
@@ -144,7 +144,8 @@ function scrape(doc, url) {
 		}
 		var description = text(doc, '#description .content')
 			|| text(doc, '#description')
-			|| text(doc, 'ytm-expandable-video-description-body-renderer .collapsed-string-container');
+			|| text(doc, 'ytm-expandable-video-description-body-renderer .collapsed-string-container')
+			|| text(doc, '#snippet span');
 		if (description) {
 			item.abstractNote = description;
 		}
